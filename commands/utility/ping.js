86e1be15fd761af3e1b871b287685f0e55a9817b
Unfire1 <slayerblade145@gmail.com>
2024-03-10 19:37:35 +0800

    Optimized ping command

--- commands/utility/ping.js
+++ commands/utility/ping.js
@@ -10,23 +10,15 @@ const replyMessage = new EmbedBuilder()
     .setDescription('Calculating ping...')
     .setFooter({ text: 'Have a nice day!' });
 
-const replyMessageEdited = new EmbedBuilder()
-    .setAuthor({
-        name : 'Camiki',
-        iconURL: 'https://cdn.discordapp.com/attachments/1200510427306676264/1212693699675557908/image.png?ex=65f2c404&is=65e04f04&hm=e145b2c6f7e6f2cc1340d87ff93f9b60944667de378e51e87b16f37374794c6a&'
-    })
-    .setColor('#ff57f6')
-    .setTitle('Ping')
-    .setFooter({ text: 'Have a nice day!' });
-
 module.exports = {
 
     data: new SlashCommandBuilder()
         .setName('ping')
         .setDescription('Determines the current status of the bot.'),
     async execute(interaction) {
-        const sent = await interaction.reply({ embeds: [replyMessage], fetchReply: true });
-        interaction.editReply({ embeds: [replyMessageEdited
+        const sent = await interaction.reply({ embeds: [replyMessage
+            .setDescription('Calculating ping...')], fetchReply: true });
+        interaction.editReply({ embeds: [replyMessage
             .setDescription(`${sent.createdTimestamp - interaction.createdTimestamp} ms`)
         ]});
     },

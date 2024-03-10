f81020b812f400522b1d1772fe8b1dd63fed76b8
Unfire1 <slayerblade145@gmail.com>
2024-03-11 00:45:57 +0800

    Attempt at implementing role color view

--- commands/utility/role.js
+++ commands/utility/role.js
@@ -1,4 +1,19 @@
-const { SlashCommandBuilder } = require('discord.js');
+const { SlashCommandBuilder, EmbedBuilder, AttachmentBuilder } = require('discord.js');
+const Jimp = require('jimp');
+
+function decimalToHexColor(decimal) {
+    var hex = Number(decimal).toString(16);
+    hex = "#000000".substring(0, 7 - hex.length) + hex;
+    return hex;
+}
+
+const embed = new EmbedBuilder()
+    .setAuthor({
+        name : 'Camiki',
+        iconURL: 'https://cdn.discordapp.com/attachments/1200510427306676264/1212693699675557908/image.png?ex=65f2c404&is=65e04f04&hm=e145b2c6f7e6f2cc1340d87ff93f9b60944667de378e51e87b16f37374794c6a&'
+    })
+    .setColor('#ff57f6')
+    .setFooter({ text: 'Have a nice day!' });
 
 module.exports = {
 
@@ -27,17 +42,29 @@ module.exports = {
 
         if (subcommandGroup === 'color') {
             if (subcommand === 'view') {
+
+                // let roleColorImageObject = new Jimp(256, 256, decimalToHexColor(interaction.member.roles.color.color), (error, image) => {
+                //     if (error) throw error;
+                //     roleColorImageObject = image.getBufferAsync(Jimp.MIME_PNG)
+                //     // .then(result => roleColorImageObject = result);
+                // });
+                // const roleColorImage = new AttachmentBuilder()
+                // .setFile(roleColorImageObject, 'roleColorImage')
+                // const replyEmbed = embed
+                // .setTitle('Current role color')
+                // .setImage('attachment://roleColorImage');
                 
-                interaction.reply({ content: "Sorry, this command is still a work in progress. Check back soon!", ephemeral: true })
+                // // interaction.reply({ embeds: [replyEmbed], files: [{ attachment: roleColorImageObject.bitmap.data, name: 'roleColorImage'}] });
+                // interaction.reply({ content: "Test", files: [{ attachment: roleColorImageObject }] })
 
             } else if (subcommand === 'change') {
 
                 interaction.member.roles.color.setColor(`${interaction.options.getString('color')}`)
                 .catch(console.error);
-                interaction.reply({ content: "Your role color has been successfully changed!" })
+                interaction.reply({ content: "Your role color has been successfully changed!" });
 
             };
         };
-    }
+    },
 
 };

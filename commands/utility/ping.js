a6917b34c7511e1a0bf335d63a94c92b00a717be
Unfire1 <slayerblade145@gmail.com>
2024-03-10 19:48:47 +0800

    Changed ping command implementation

--- commands/utility/ping.js
+++ commands/utility/ping.js
@@ -1,13 +1,12 @@
 const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
 
-const replyMessage = new EmbedBuilder()
+const pingEmbed = new EmbedBuilder()
     .setAuthor({
         name : 'Camiki',
         iconURL: 'https://cdn.discordapp.com/attachments/1200510427306676264/1212693699675557908/image.png?ex=65f2c404&is=65e04f04&hm=e145b2c6f7e6f2cc1340d87ff93f9b60944667de378e51e87b16f37374794c6a&'
     })
     .setColor('#ff57f6')
     .setTitle('Ping')
-    .setDescription('Calculating ping...')
     .setFooter({ text: 'Have a nice day!' });
 
 module.exports = {
@@ -16,9 +15,9 @@ module.exports = {
         .setName('ping')
         .setDescription('Determines the current status of the bot.'),
     async execute(interaction) {
-        const sent = await interaction.reply({ embeds: [replyMessage
+        const sent = await interaction.reply({ embeds: [pingEmbed
             .setDescription('Calculating ping...')], fetchReply: true });
-        interaction.editReply({ embeds: [replyMessage
+        interaction.editReply({ embeds: [pingEmbed
             .setDescription(`${sent.createdTimestamp - interaction.createdTimestamp} ms`)
         ]});
     },

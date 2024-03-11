ffb8013b7199fc86a393fb4d03f31ca3f4b8853b
Unfire1 <slayerblade145@gmail.com>
2024-03-11 20:08:55 +0800

    Slightly changed image URL

--- commands/utility/ping.js
+++ commands/utility/ping.js
@@ -3,7 +3,7 @@ const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
 const pingEmbed = new EmbedBuilder()
     .setAuthor({
         name : 'Camiki',
-        iconURL: 'https://cdn.discordapp.com/attachments/1200510427306676264/1212693699675557908/image.png?ex=65f2c404&is=65e04f04&hm=e145b2c6f7e6f2cc1340d87ff93f9b60944667de378e51e87b16f37374794c6a&'
+        iconURL: 'https://cdn.discordapp.com/attachments/1200510427306676264/1212693699675557908/image.png'
     })
     .setColor('#ff57f6')
     .setTitle('Ping')

bfdc70ef25dee25fa12943be5a8e464a0a5ec947
Unfire1 <slayerblade145@gmail.com>
2024-03-18 00:18:57 +0800

    Renamed inspire to insight

--- commands/fun/insight.js
+++ commands/fun/insight.js
@@ -9,11 +9,23 @@ const embed = new EmbedBuilder()
     .setColor('#ff57f6')
     .setFooter({ text: 'Have a nice day!' });
 
+const personalQuotes = [
+    'Not everyone is born into a world of privilege and position, where they are not chained by the economical struggles that burden the rest of us.',
+    'Passion is a flame that burns on a foundation of freedom, yet the human condition of many is poised atop a rocky ledge, ever close to tipping into the dark abyss below.',
+    'No rational person would willfully submit themselves to a risky endeavor, when there exists an inherently better option, grounded upon the realities of human life. That is but a gamble only the rich can afford to bet on.',
+    'We must appreciate all those that speak and act with a fiery passion, for they are often subject to underpayment and underappreciation, a life of toil for which they see no fruits, for their labor is but an accepted norm, and no psychological considerations are made for they who can no longer exert the force and effort demanded of them, from top and bottom.',
+    'Live and let live, for we are but hatchlings in the same nest.',
+    'It is only people who have no experience, who think that every problem has a simple and easy solution. These people are, more often than not, keyboard warriors, people who endlessly complain, yet idle and do nothing to help fight the fire they light up.',
+    'A bad apple has no right representing the majority, yet everyone cherry picks examples of bad faith actors, while few go through the struggle of reforming society, even if through small acts of good faith.',
+    'We are all human, surely we can work out our differences, and show our gratitude to the ones fighting for us all?',
+    'Most of passion fall prey to the predator that is market forces.'
+]
+
 module.exports = {
 
     data: new SlashCommandBuilder()
-        .setName('inspire')
-        .setDescription('Get an inspirational quote.'),
+        .setName('insight')
+        .setDescription('Get an insightful quote.'),
 
     async execute(interaction) {
 
@@ -27,7 +39,7 @@ module.exports = {
             .then(data => {
                 
                 const imageWidth = 700;
-                const imageHeight = 500;
+                const imageHeight = 1000;
                 const textBufferX = 20;
                 const textBufferY = 12;
                 const extraBottomBuffer = 2;

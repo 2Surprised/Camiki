3d1ecdc3b454ce8ea1fd15e877a21e9ced7c361d
Unfire1 <slayerblade145@gmail.com>
2024-03-17 18:58:54 +0800

    Finished necessary text alignment logic

--- commands/fun/inspire.js
+++ commands/fun/inspire.js
@@ -18,19 +18,64 @@ module.exports = {
             })
             .then(data => {
                 
-                new Jimp(1000, 500, '#FF00FF', (error, image) => {
+                const imageWidth = 700;
+                const imageHeight = 500;
+                const textBufferX = 20;
+                const textBufferY = 16;
+                let finalHeight = textBufferY;
+
+                new Jimp(imageWidth, imageHeight, '#FFFFFF', (error, image) => {
 
                     if (error) throw error;
                     Jimp.loadFont(Jimp.FONT_SANS_32_BLACK).then(font => {
-                        image.print(font, 200, 200, 'Hello world!')
+
+                        image.print( // Deals with the main body of text
+                            font,
+                            textBufferX,
+                            textBufferY,
+                            `${data.text}`,
+                            imageWidth - (textBufferX * 2),
+                            imageHeight - (textBufferY * 2),
+
+                            (error, image, {x, y}) => {
+                                if (error) throw error;
+
+                                image.print( // Deals with the display of the author's name
+                                    font,
+                                    -textBufferX,
+                                    y + 10,
+                                    { text: `- ${data.author.name}`, alignmentX: Jimp.HORIZONTAL_ALIGN_RIGHT },
+                                    imageWidth,
+                                    imageHeight,
+
+                                    (error, image, {x, y}) => {
+                                        if (error) throw error;
+                                        finalHeight += y
+                                    }
+                                )
+                            }
+                        )
+
+                        // image.crop(
+                        //     0,
+                        //     finalHeight,
+                        //     0,
+                        //     imageHeight - finalHeight
+                        // )
+
                         image.getBuffer(Jimp.MIME_PNG, (error, imageFile) => {
                             if (error) throw error;
-                            interaction.reply({ files: [imageFile] })
+                            interaction.reply({ files: [imageFile] });
                         });
+
                     });
 
                 });
 
+            })
+            .catch(error => {
+                console.error(error)
+                interaction.reply({ content: 'Sorry, the API could not be reached. Try again!', ephemeral: true })
             })
 
     }

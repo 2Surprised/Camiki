792f95ecf6b3a6adee6caca32895c8d5aecab2d0
Unfire1 <slayerblade145@gmail.com>
2024-03-17 21:53:01 +0800

    Removed unused module

--- commands/fun/apod.js
+++ commands/fun/apod.js
@@ -1,4 +1,4 @@
-const { SlashCommandBuilder, EmbedBuilder, AttachmentBuilder } = require('discord.js');
+const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
 const { nasaAPIkey } = require('../../config.json')
 
 module.exports = {

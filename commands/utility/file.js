81fe9c91c2603f05b53b5e211c95771456b6eb19
Unfire1 <slayerblade145@gmail.com>
2024-03-27 04:09:00 +0800

    Finished implementation of file convert command

--- commands/utility/file.js
+++ commands/utility/file.js
@@ -27,27 +27,42 @@ module.exports = {
 
     async execute(interaction) {
 
+        const errorDefault = 'Something went wrong, try again!';
+        const errorAdvice = 'If issues persist, then the desired file format might not be compatible with the uploaded attachment, or the file size might be too large for Camiki to reupload.';
+
         interaction.deferReply()
             .catch(error => console.error(error));
 
         try {
             
+            const userAttachment = interaction.options.getAttachment('from');
+            const attachedURL = userAttachment.url;
+            const originalFormat = userAttachment.name.split('.').slice(-1).join('');
+            const wantedFormat = interaction.options.getString('to').toLowerCase();
+
             const pathToStore = 'Related/Files/';
-            const inputName = 'input.wmv';
-            const outputName = 'output.mp4';
+            const inputName = `${Date.now()}.${originalFormat}`;
+            const outputName = `${Date.now()}.${wantedFormat}`;
             const inputPath = pathToStore + inputName;
             const outputPath = pathToStore + outputName;
-            const userAttachment = interaction.options.getAttachment('from').url;
 
             let response;
             await (async () => {
-                response = await fetch(userAttachment);
+                response = await fetch(attachedURL);
             })();
-
             const buffer = await consumers.buffer(response.body);
             fs.writeFileSync(inputPath, buffer);
+
             const ffmpegCommand = `ffmpeg -i ${inputPath} ${outputPath}`;
-            const ffmpegProcess = spawn(ffmpegCommand, { shell: true });
+            let ffmpegProcess;
+            try {
+                ffmpegProcess = spawn(ffmpegCommand, { shell: true });
+            } catch (error) {
+                await (async () => {
+                    fs.unlink(inputPath, (error) => { if (error) { throw error }; });
+                })();
+                throw error;
+            }
     
             // I have no idea why it's stderr
             ffmpegProcess.stderr.once('data', data => {
@@ -59,19 +74,25 @@ module.exports = {
     
             ffmpegProcess.on('close', code => {
                 if (code !== 0) {
-                    interaction.editReply({ content: 'Sorry, the conversion has failed!' });
+                    interaction.editReply({ content: `Sorry, the conversion has failed! ${errorAdvice}` });
+                    fs.unlink(inputPath, (error) => { if (error) { throw error }; });
                 } else if (code === 0) {
                     const convertedFile = new AttachmentBuilder().setFile(outputPath);
-                    interaction.editReply({ content: 'Here is the output file after conversion!', files: [convertedFile] })
+                    interaction.editReply({ content: `Here is the output file after conversion, in ${wantedFormat.toUpperCase()} format!`, files: [convertedFile] })
                         .then(response => {
-                            fs.unlink(inputPath, (error) => { if (error) throw error; });
-                            fs.unlink(outputPath, (error) => { if (error) throw error; });
+                            fs.unlink(inputPath, (error) => { if (error) { throw error }; });
+                            fs.unlink(outputPath, (error) => { if (error) { throw error }; });
+                        })
+                        .catch(error => {
+                            console.error(error);
+                            interaction.editReply({ content: `${errorDefault} ${errorAdvice}` })
                         })
                 };
             });
+
         } catch (error) {
             console.log(error);
-            interaction.followUp('Sorry, something went wrong during the download and conversion process!');
+            interaction.followUp(`${errorDefault} ${errorAdvice}`);
         };
 
     },

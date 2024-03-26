998f793e43269ab315830546d3615862e2de137f
Unfire1 <slayerblade145@gmail.com>
2024-03-27 01:33:37 +0800

    Mostly finished work

--- commands/utility/file.js
+++ commands/utility/file.js
@@ -1,5 +1,8 @@
-const { SlashCommandBuilder } = require('discord.js');
+const { SlashCommandBuilder, AttachmentBuilder } = require('discord.js');
+const consumers = require('stream/consumers');
+const fs = require('fs');
 const { spawn } = require('node:child_process');
+const path = require('path');
 
 module.exports = {
 
@@ -23,29 +26,50 @@ module.exports = {
 
     async execute(interaction) {
 
-        const inputFile = interaction.options.getAttachment('from');
-        const outputFile = 'output.mp4';
-        const ffmpegCommand = `ffmpeg -i ${inputFile} ${outputFile}`;
-        const ffmpegProcess = spawn(ffmpegCommand, { shell: true });
+        interaction.deferReply()
+            .catch(error => console.error(error));
 
-        console.log(inputFile);
+        try {
+            
+            const pathToStore = 'Related/Files/';
+            const inputName = 'input.wmv';
+            const outputName = 'output.mp4';
+            const inputFile = pathToStore + inputName;
+            const outputFile = pathToStore + outputName;
+            const userAttachment = interaction.options.getAttachment('from').url;
 
-        ffmpegProcess.stdin.on('data', data => {
-            console.log('teehee')
-            console.log(data);
-        });
+            let response;
+            await (async () => {
+                response = await fetch(userAttachment);
+            })();
 
-        ffmpegProcess.stderr.on('data', data => {
-            console.error('FFmpeg Error:', data);
-        });
-
-        ffmpegProcess.on('close', code => {
-            if (code === 0) {
-                console.log('Conversion successful!');
-            } else {
-                console.log('Conversion failed.');
-            };
-        });
+            const buffer = await consumers.buffer(response.body);
+            fs.writeFileSync(inputFile, buffer);
+            const ffmpegCommand = `ffmpeg -i ${inputFile} ${outputFile}`;
+            const ffmpegProcess = spawn(ffmpegCommand, { shell: true });
+    
+            // I have no idea why it's stderr
+            ffmpegProcess.stderr.once('data', data => {
+                interaction.followUp('Your file is being converted, please be patient while it processes!')
+            })
+            ffmpegProcess.stderr.on('data', data => {
+                console.log('Processing:', data);
+            });
+    
+            ffmpegProcess.on('close', code => {
+                if (code !== 0) {
+                    console.log('Conversion failed.');
+                    interaction.followUp({ content: 'Sorry, the conversion has failed!', ephemeral: true });
+                } else if (code === 0) {
+                    console.log('Conversion success!')
+                    const convertedFile = new AttachmentBuilder().setFile(`../../Related/Files/${outputName}`);
+                    interaction.followUp({ content: 'Here is the output file after conversion!', files: [convertedFile] })
+                };
+            });
+        } catch (error) {
+            console.log(error);
+            interaction.followUp('Sorry, something went wrong during the download and conversion process!');
+        };
 
     },
 

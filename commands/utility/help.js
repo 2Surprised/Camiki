8161a5297400f222b4a6e7693f5de69abf97f4e2
Unfire1 <slayerblade145@gmail.com>
2024-04-03 23:40:38 +0800

    Added gemini command (it doesn't actually work)

--- commands/utility/help.js
+++ commands/utility/help.js
@@ -8,7 +8,7 @@ const helpEmbed = new EmbedBuilder()
     .setColor('#ff57f6')
     .addFields(
         { name: 'Utility commands', value: '`/help`, `/ping`, `/user info`, `/user avatar`, `/role color view`, `/role color change`, `/file convert`' },
-        { name: 'Fun commands', value: '`/apod`, `/insight`, `/birthday`' }
+        { name: 'Fun commands', value: '`/apod`, `/insight`, `/birthday`, `/gemini`' }
     )
     .setFooter({ text: 'Have a nice day!' });
 
